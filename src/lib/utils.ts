93ba055c7a2ab521409df3ef15392715a0185822
Tayler The Kid <126191293+Tayler01@users.noreply.github.com>
2025-06-28 14:29:33 -0400

    Add missing cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,10 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 import type { ChatMessage } from './supabase'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export function formatTime(date: string | Date) {
   const d = new Date(date)
